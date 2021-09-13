bc5e2256f61691659cfba2d91ed8aa72315d3d47
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-09-13 02:01:13 -0700

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner sc= new Scanner(System.in);
+        String noun, verb, adjective, adverb;
+        String author;
+        System.out.print("Enter a noun: ");
+        noun=sc.nextLine();
+        System.out.print("Enter a verb: ");
+        verb=sc.nextLine();
+        System.out.print("Enter an adjective: ");
+        adjective=sc.nextLine();
+        System.out.print("Enter an adverb: ");
+        adverb=sc.nextLine();
+        System.out.println("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
+    }
+}
